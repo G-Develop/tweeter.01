31b71f0319eb05c7411dfdab16fc9fb40866563d
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-09-11 22:34:35 +0000

    char counter with colour implemented

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,18 @@
-console.log('char counter loaded')
-
 $(document).ready(function() {
   console.log('document is ready')
-  $("textarea").keypress(function() {
-    alert($(this).val());
+  $("textarea").keyup(function() {
+    let TextareaLength = $(this).val().length;
+    let dotCounter = $(this).siblings(".counter");
+    let maxChar = 140 - TextareaLength;
+    dotCounter.text(maxChar);
+    if (maxChar < 0 ) {
+      dotCounter.css("color", "red");
+    } else {
+      dotCounter.css("color", "black");
+    }
   })
 
-
-
-
-
+console.log($(".counter").text())
 
 
 });
