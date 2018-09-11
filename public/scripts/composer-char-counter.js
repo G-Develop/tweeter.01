04bd2988ac622d0f225d169f4a1f90b765d65eee
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-09-11 21:17:26 +0000

    added the jquery event handler

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,4 +2,14 @@ console.log('char counter loaded')
 
 $(document).ready(function() {
   console.log('document is ready')
+  $("textarea").keypress(() => {
+    alert("test");
+  })
+
+
+
+
+
+
+
 });
