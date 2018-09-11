0583a6f9dbeb0187183e1930f61c9731ceeb753e
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-09-11 21:31:02 +0000

    used alert().val() to be able to call this using jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,8 @@ console.log('char counter loaded')
 
 $(document).ready(function() {
   console.log('document is ready')
-  $("textarea").keypress(() => {
-    alert("test");
+  $("textarea").keypress(function() {
+    alert($(this).val());
   })
 
 
